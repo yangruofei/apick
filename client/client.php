5fb1724ad3e81cb8c3115a5bae508773c8bbe6f0
yrf <1012180174@qq.com>
2018-09-19 14:22:57 +0800

    数据标准化输出完毕

--- client/client.php
+++ client/client.php
@@ -1,4 +1,16 @@
 <?php
-	//客户端发起api请求
+	include "./vendor/autoload.php";
 
+	//请求的api地址
+	$url="http://localhost/api/server/server.php";
+
+	$curl = new Curl\Curl();
+	$curl->get($url);
+
+	if ($curl->error) {
+	    echo $curl->error_code;
+	}else{
+	    echo $curl->response;
+	}
+	//客户端发起api请求
 ?>
--- server/server.php
+++ server/server.php
@@ -1,4 +1,19 @@
 <?php
 	//接收请求响应数据 json
+	$pdo=new PDO("mysql:host=localhost;dbname=api;charset=utf8","root","413466");
+	$stmt=$pdo->query("select * from user");
+	$data=$stmt->fetchAll(PDO::FETCH_ASSOC);
 
+	//第一标准化产出数据格式函数
+	function resp($data,$status,$message){
+		$res=[
+			"status"=>$status,//服务器响应状态码  20
+			"message"=>$message,//此次api请求的描述
+			"data"=>$data
+		];
+		echo json_encode($res,JSON_UNESCAPED_UNICODE);
+	}
+	
+	resp($data,200,"ok");
+	
 ?>
