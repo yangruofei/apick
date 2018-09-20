caff3012bd93ae40073bb0b1fae77e93d261887f
yrf <1012180174@qq.com>
2018-09-20 15:27:52 +0800

    解决恶意多次请求api的问题

--- server/checksing.php
+++ server/checksing.php
@@ -1,13 +1,17 @@
 <?php
 	include "./aes.php";
 
+	//使用redis存储$sing
+	$red=new Redis();
+	$red->connect("127.0.0.1",6379);
+
 	//检测秘钥
 	try{
 		if(empty($_GET["sing"])){
 			throw new Exception("加密签名不存在");
 		}
 		
-		check($_GET["sing"]);
+		check($_GET["sing"],$red);
 		
 	}catch(Exception $e){
 		echo resp([],401,$e->getMessage());
@@ -15,7 +19,7 @@
 	}
 
 	//校验签名
-	function check($sing){
+	function check($sing,$red){
 		$aes = new AESMcrypt($bit = 128, $key = 'abcdef1234567890', $iv = '0987654321fedcba', $mode = 'cbc');
 		
 		// echo $sing."<br>";
@@ -26,6 +30,8 @@
 		$sing_tmp=$aes->decrypt($sing);
 		//分割字符串
 		$sing_arr=explode("/",$sing_tmp);
+
+		//用户身份识别
 		if($sing_arr[0]!=Token){
 			throw new Exception("加密签名不正确");
 		}
@@ -34,6 +40,15 @@
 		if((time()-$sing_arr[1])>5){
 			throw new Exception("加密签名失效");
 		}
+		echo $sing."<br>";
+		//多次请求
+		if($red->get($sing)){
+			throw new Exception("签名已经被使用");
+			exit;
+		}
+
+		//第二个参数可以随便设置
+		$red->set($sing,"true");
 		
 	}
 ?>
