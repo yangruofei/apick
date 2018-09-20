0d1f7f8f3872bc1ed3506a5b7ad201107e537576
yrf <1012180174@qq.com>
2018-09-20 13:42:16 +0800

    使用时间控制api请求是否有效来解决api暴露问题

--- client/setsing.php
+++ client/setsing.php
@@ -3,7 +3,8 @@
 	//加密签名
 	function setsing($token){
 		$aes = new AESMcrypt($bit = 128, $key = 'abcdef1234567890', $iv = '0987654321fedcba', $mode = 'cbc');
-		$sing=$aes->encrypt($token);
+		$sing=$token."/".time();
+		$sing=$aes->encrypt($sing);
 		return $sing;
 	}
 ?>
--- server/checksing.php
+++ server/checksing.php
@@ -17,8 +17,22 @@
 	//校验签名
 	function check($sing){
 		$aes = new AESMcrypt($bit = 128, $key = 'abcdef1234567890', $iv = '0987654321fedcba', $mode = 'cbc');
-		if($aes->decrypt($sing)!=Token){
+		
+		// echo $sing."<br>";
+
+		//将sing中的空格替换为+号
+		$sing=str_replace(" ","+",$sing);
+
+		$sing_tmp=$aes->decrypt($sing);
+		//分割字符串
+		$sing_arr=explode("/",$sing_tmp);
+		if($sing_arr[0]!=Token){
 			throw new Exception("加密签名不正确");
+		}
+
+		//防止api暴露
+		if((time()-$sing_arr[1])>5){
+			throw new Exception("加密签名失效");
 		}
 		
 	}
